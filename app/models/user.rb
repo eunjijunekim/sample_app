192027eedf120e7d86885c53445440cd387cc0a1
Eun Ji Kim <eunji.june.kim@gmail.com>
2013-09-11 16:09:07 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,13 @@ class User < ActiveRecord::Base
 	uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }
 	has_secure_password
+	has_many :microposts, dependent: :destroy
 	def User.new_remember_token
     	SecureRandom.urlsafe_base64
+  	end
+
+  	def feed
+  		Micropost.where("user_id = ?" , id)
   	end
 
   	def User.encrypt(token)
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,6 +48,18 @@ describe "Authentication" do
 
   describe "authorization" do
 
+    describe "as non-admin user" do
+      let(:user) {FactoryGirl.create(:user)}
+      let(:non_admin) {FactoryGirl.create(:user)}
+
+      before {sign_in non_admin, no_capybara: true}
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before {delete user_path(user)}
+        specify {expect(response).to redirect_to(root_url)}
+      end
+    end
+
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -82,6 +94,18 @@ describe "Authentication" do
         describe "visiting the user index" do
           before {visit users_path}
           it {should have_title('Sign in')}
+        end
+      end
+
+      describe "in the Microposts controller" do
+        describe "submitting to the create action" do
+          before {post microposts_path}
+          specify {expect(response).to redirect_to(signin_path)}
+        end
+
+        describe "submitting to the destroy action" do
+          before {delete micropost_path(FactoryGirl.create(:micropost))}
+          specify {expect(response).to redirect_to(signin_path)}
         end
       end
     end
